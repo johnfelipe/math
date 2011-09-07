5c89c3bedd29f382ff4585b74718d86fddbefe23
eric <wilson.eric.n@gmail.com>
2011-09-07 06:19:44 -0400

    Added generateFacts method

--- facts-test.py
+++ facts-test.py
@@ -10,7 +10,7 @@ class FactsTest(unittest.TestCase):
         self.assertEquals(3,fact.left)
         self.assertEquals(5,fact.right)
         self.assertEquals(8,fact.result)
-        self.assertEquals('+',str(fact.op))
+        self.assertEquals('+',fact.sign)
 
     def testMakeMinus(self):
         fact = facts.makeFact(9,5,minus)
@@ -18,12 +18,58 @@ class FactsTest(unittest.TestCase):
         self.assertEquals(9,fact.left)
         self.assertEquals(5,fact.right)
         self.assertEquals(4,fact.result)
-        self.assertEquals('-',str(fact.op))
+        self.assertEquals('-',fact.sign)
 
     def testChoosePlusMinus(self):
         op = facts.choosePlusMinus()
 
         self.assertTrue(str(op) == '+' or str(op) == '-')
+
+    def testGenerateFactsStaysWithinLimits(self):
+        high = 15
+        low = 2
+        amount = 500
+        
+        results = facts.generateFacts(low, high, amount)
+
+        for fact in results:
+            self.assertTrue(low <= fact.left)
+            self.assertTrue(low <= fact.right)
+            self.assertTrue(high >= fact.left)
+            self.assertTrue(high >= fact.right)
+
+    def testGenerateFactsDefaultsToZeroToTwelve(self):
+        high = 12
+        low = 0
+        amount = 500
+        
+        results = facts.generateFacts(low, high, amount)
+
+        for fact in results:
+            self.assertTrue(low <= fact.left)
+            self.assertTrue(low <= fact.right)
+            self.assertTrue(high >= fact.left)
+            self.assertTrue(high >= fact.right)
+
+    def testGenerateFactsDefaultsThirtyFacts(self):
+        high = 12
+        low = 0
+        
+        results = facts.generateFacts(low, high)
+
+        self.assertEquals(30,len(results))
+
+    def testGenerateFactsCanGiveMinus(self):
+        results = facts.generateFacts(op=minus)
+
+        for fact in results:
+            self.assertEquals('-',fact.sign)
+
+    def testGenerateFactsCanGivePlus(self):
+        results = facts.generateFacts(op=plus)
+
+        for fact in results:
+            self.assertEquals('+',fact.sign)
 
 if __name__ == '__main__':
     unittest.main()
--- facts.py
+++ facts.py
@@ -1,12 +1,20 @@
 from collections import namedtuple
-from random import choice
-from operations import plsmns
+from random import choice, randint
+from operations import plsmns,plus,minus
 
-Fact = namedtuple('Fact','left,right,result,op')
+Fact = namedtuple('Fact','left,right,result,sign')
 
 def makeFact(left,right,op):
     result = op.do(left,right)
-    return Fact(left,right,result,op)
+    return Fact(left,right,result,str(op))
 
 def choosePlusMinus():
     return choice(plsmns)   
+
+def generateFacts(low=0, high=12, amount=30, op=plus):
+    return [generateFact(low,high,op) for _ in range(amount) ]
+
+def generateFact(low,high,op):
+    left = randint(low,high)
+    right = randint(low,high)
+    return makeFact(left,right,op)
