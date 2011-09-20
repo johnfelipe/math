92eab6590a363557f79a0dd2e875418fda5c2193
eric <wilson.eric.n@gmail.com>
2011-09-20 06:32:32 -0400

    added latex munging

--- latex-test.py
+++ latex-test.py
@@ -21,6 +21,67 @@ class LatexTest(unittest.TestCase):
 
         self.assertEquals(expected,texfact)
     
+    def testMakeRow(self):
+        row = [ 'one', 'two', 'three' ]
+        expected = r'''\[ one \hspace{1in} two \hspace{1in} three \]
+'''
+    
+    def testMakeSpacedRows(self):
+        rows = [ 'one\n', 'two\n', 'three\n' ]
+        expected = r'''one
+
+\vspace{0.75cm}
+
+two
+
+\vspace{0.75cm}
+
+three
+'''
+
+        texrows = latex.typesetspacedrows(rows)
+
+        self.assertEquals(expected,texrows)
+
+    def testMakeHeader(self):
+        expected = r'''\documentclass{article}
+
+\usepackage[hmargin=1in,vmargin=1in]{geometry}
+\usepackage{xlop}
+
+\begin{document}
+\thispagestyle{empty}
+\title{Arithmetic Practice}
+\author{WCI}
+
+%\maketitle
+\newcommand\hole[1]{\texttt{\ }}
+'''
+
+        header = latex.makeheader()
+
+        self.assertEquals(expected, header)
+
+    def testMakeLine(self):
+        expected = r'''\vspace{1cm}
+\begin{center}
+\line(1,0){450}
+\end{center}
+'''
+
+        line = latex.makeline()
+
+        self.assertEquals(expected, line)
+
+    def testMakeEnd(self):
+        expected = r'''
+\end{document}
+'''
+
+        end = latex.makeend()
+
+        self.assertEquals(expected, end)
+
 if __name__ == '__main__':
     unittest.main()
 
--- latex.py
+++ latex.py
@@ -6,3 +6,41 @@ def typesetfact(fact):
     style = '[resultstyle=\hole,carrystyle=\hole]'
     nums = '{%d}{%d}' % (fact.left, fact.right)
     return op + style + nums
+
+def typesetrow(facts):
+    hspace = r' \hspace{1in} '
+    dispstart = r'\['
+    dispend = r'\]' + '\n'
+    dispfacts = hspace.join(facts)
+    return ' '.join([dispstart,dispfacts,dispend])
+
+def typesetspacedrows(rows):
+    vspace = '\n' r'\vspace{0.75cm}' + '\n\n'
+    return vspace.join(rows)
+
+def makeheader():
+    return r'''\documentclass{article}
+
+\usepackage[hmargin=1in,vmargin=1in]{geometry}
+\usepackage{xlop}
+
+\begin{document}
+\thispagestyle{empty}
+\title{Arithmetic Practice}
+\author{WCI}
+
+%\maketitle
+\newcommand\hole[1]{\texttt{\ }}
+'''
+
+def makeline():
+    return r'''\vspace{1cm}
+\begin{center}
+\line(1,0){450}
+\end{center}
+'''
+
+def makeend():
+    return r'''
+\end{document}
+'''
